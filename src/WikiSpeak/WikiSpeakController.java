2969fdadcc9b096c42cf3688c124019cdb496afb
Callum <cdan879@aucklanduni.ac.nz>
2019-09-17 16:39:25 +1200

    Rename package and controller Change Main window default size Link Main window to main.fxml Add id's to elements in main.fxml, link to WikiSpeakController and link buttons to handlers Add handlers to WikiSpeakController

---
+++ src/WikiSpeak/WikiSpeakController.java
@@ -0,0 +1,34 @@
+package WikiSpeak;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+
+public class WikiSpeakController {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private ListView creationsList;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button newCreationButton;
+
+    @FXML
+    protected void handlePlayButton(ActionEvent event) {
+    }
+
+    @FXML
+    public void handleNewCreationButton(ActionEvent event) {
+    }
+
+    @FXML
+    public void handleDeleteButton(ActionEvent event) {
+    }
+}
