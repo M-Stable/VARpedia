91d833fe7816dcbec8c13b14ab5f2b55e51c8b68
M-Stable <fchanglee@gmail.com>
2019-09-23 21:23:18 +1200

    Got New Creation button on Main Menu to switch scene

--- src/WikiSpeak/WikiSpeakController.java
+++ src/WikiSpeak/WikiSpeakController.java
@@ -2,8 +2,15 @@ package WikiSpeak;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class WikiSpeakController {
@@ -25,7 +32,14 @@ public class WikiSpeakController {
     }
 
     @FXML
-    public void handleNewCreationButton(ActionEvent event) {
+    public void handleNewCreationButton(ActionEvent event) throws IOException {
+        Parent creationParent = FXMLLoader.load(getClass().getResource("creator.fxml"));
+        Scene newCreationScene = new Scene(creationParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newCreationScene);
+        window.show();
+
     }
 
     @FXML
