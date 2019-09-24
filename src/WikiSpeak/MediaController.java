f600a8c34a2f42e45cc03ca16bee3d88321b7020
crazyclerkm <cdan879@aucklanduni.ac.nz>
2019-09-24 14:50:51 +1200

    Implement media scene functionality

--- src/WikiSpeak/MediaController.java
+++ src/WikiSpeak/MediaController.java
@@ -1,21 +1,71 @@
 package WikiSpeak;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
 
-public class MediaController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MediaController implements Initializable{
 
     @FXML
     private Slider volumeSlider;
 
     @FXML
-    private MediaView mediaView;
+    private BorderPane parentPane;
+
+    private MediaPlayer player;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        MediaView mediaView = (MediaView) parentPane.lookup("#mediaView");
+        player = mediaView.getMediaPlayer();
+
+        volumeSlider.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                if (volumeSlider.isValueChanging()) {
+                    player.setVolume(volumeSlider.getValue());
+                }
+
+            }
+        });
+
+    }
 
     public void handlePlayButton(ActionEvent actionEvent) {
+        if(player.getStatus() == MediaPlayer.Status.PLAYING) {
+            player.pause();
+        } else {
+            player.play();
+        }
+
+
     }
 
-    public void handleBackButton(ActionEvent actionEvent) {
+    public void handleBackButton(ActionEvent actionEvent) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene mainMenu = new Scene(mainParent);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(mainMenu);
+        window.show();
     }
+
+
 }
--- src/WikiSpeak/WikiSpeakController.java
+++ src/WikiSpeak/WikiSpeakController.java
@@ -6,24 +6,55 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 
 public class WikiSpeakController {
 
     @FXML
+    private ListView creationsList;
+
+    @FXML
     protected void handlePlayButton(ActionEvent event) throws IOException {
 
-        Parent creationParent = FXMLLoader.load(getClass().getResource("media.fxml"));
-        Scene newCreationScene = new Scene(creationParent);
+        if(creationsList.getSelectionModel().getSelectedItem() == null) {
 
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(newCreationScene);
-        window.show();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No Creation selected");
+            alert.show();
+
+        } else {
+
+            File videoFile = (File) creationsList.getSelectionModel().getSelectedItem();
+
+            Media video = new Media(videoFile.toURI().toString());
+            MediaPlayer player = new MediaPlayer(video);
+            MediaView mediaView = new MediaView(player);
+            mediaView.setId("mediaView");
+
+            BorderPane creationParent = FXMLLoader.load(getClass().getResource("media.fxml"));
+            Scene newCreationScene = new Scene(creationParent);
+
+            creationParent.setCenter(mediaView);
+
+            //creationParent.getChildren().add(mediaView);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(newCreationScene);
+            window.show();
+        }
+
+
     }
 
     @FXML
@@ -39,5 +70,9 @@ public class WikiSpeakController {
 
     @FXML
     public void handleDeleteButton(ActionEvent event) {
+
+        File testvid = new File("test.mp4");
+        creationsList.getItems().add(testvid);
+
     }
 }
