e80acf4f20a8e6b18bd840c71cc30dcae38ff225
M-Stable <fchanglee@gmail.com>
2019-09-24 13:07:40 +1200

    added back functionality in media controller

--- src/WikiSpeak/MediaController.java
+++ src/WikiSpeak/MediaController.java
@@ -2,8 +2,15 @@ package WikiSpeak;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MediaController {
 
@@ -16,6 +23,12 @@ public class MediaController {
     public void handlePlayButton(ActionEvent actionEvent) {
     }
 
-    public void handleBackButton(ActionEvent actionEvent) {
+    public void handleBackButton(ActionEvent event) throws IOException {
+        Parent mainParent = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene mainMenu = new Scene(mainParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mainMenu);
+        window.show();
     }
 }
