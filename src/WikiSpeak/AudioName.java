585860ebba7ad04902b0862e0b2690dbf99ccd7e
M-Stable <fchanglee@gmail.com>
2019-09-28 23:12:13 +1200

    Fixed reset after creation made Save Audio name bound to ENTER key

--- src/WikiSpeak/AudioName.java
+++ src/WikiSpeak/AudioName.java
@@ -1,11 +1,14 @@
 package WikiSpeak;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -20,8 +23,17 @@ public class AudioName {
 
         TextField textField = new TextField();
 
+
         Label label = new Label("What would you like to name your audio?");
         Button btnYes = new Button("confirm");
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                    btnYes.fire();
+                }
+            }
+        });
         btnYes.setOnAction(e -> alert.close());
 
         VBox layout = new VBox(10);
