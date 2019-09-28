b5e3412575e78ebe56e214392c3784849606e41a
crazyclerkm <cdan879@aucklanduni.ac.nz>
2019-09-28 23:09:12 +1200

    Fix text sizing on creations

--- src/WikiSpeak/VideoCreationTask.java
+++ src/WikiSpeak/VideoCreationTask.java
@@ -23,30 +23,26 @@ public class VideoCreationTask extends Task<String> {
 
     @Override
     protected String call() throws Exception {
-        String output = "";
         double imageFrameRate = 1 / (audioDuration / numImages);
-        String filter = "-vf \"scale=-2:202, drawtext=fontfile=./myfont.ttf:fontsize=50: fontcolor=red:x=trunc((w-text_w)/2):y=trunc((h-text_h)/2):text='" + creationName + "\"";
+        String filter2 = "-vf \"drawtext=fontfile=./myfont.ttf:fontsize=50: fontcolor=red:x=(w-text_w)/2:y=(h-text_h)/2:text='" + creationName + "'\" ";
+        String filter = "-vf \"scale=-2:202\"";
 
         String input = "cat images/*.jpg | ffmpeg -f image2pipe -framerate " + imageFrameRate + " -i - -i creations/merged.wav -c:v libx264 -pix_fmt yuv420p  ";
 
-        String command = input + filter + " -r 25 -max_muxing_queue_size 1024 -y creations/" + creationName + ".mp4";
+        String command = input + filter + " -r 25 -max_muxing_queue_size 1024 -y creations/out.mp4";
+        String command2 = "ffmpeg -i creations/out.mp4 " + filter2 + "creations/" + creationName + ".mp4";
 
         ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
         Process process = pb.start();
 
-        BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        process.waitFor();
 
-        int exitStatus = process.waitFor();
+        ProcessBuilder pb2 = new ProcessBuilder("bash", "-c", command2);
+        Process process2 = pb2.start();
 
-        if (exitStatus == 0) {
-            output = stdout.readLine();
-        } else {
-            String line;
-            while ((line = stderr.readLine()) != null) {
-                System.err.println(line);
-            }
-        }
-        return output;
+
+        process2.waitFor();
+
+        return null;
     }
 }
